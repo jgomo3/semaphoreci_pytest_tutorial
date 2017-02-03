5dc473a73491038d247fd6d3a677976c7943d6df
Jesús Gómez <jgomo3@gmail.com>
2017-02-03 09:46:19 -0400

    Fix 1 extra credit bug

--- wallet.py
+++ wallet.py
@@ -2,12 +2,12 @@ class InsufficientAmount(Exception):
     pass
 
 class Wallet(object):
-    def __init__(self, initial_amount=1):
+    def __init__(self, initial_amount=0):
         self.balance = initial_amount
 
     def spend_cash(self, amount):
         if self.balance < amount:
-            raise InsufficientAmount(f'Not enough available to spend {amount}')
+            raise InsufficientAmount(f'Not enough available to spend {amount}!')
         self.balance -= amount
 
     def add_cash(self, amount):
